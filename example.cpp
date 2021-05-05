06de671f51e6a4556ce4f5a13ef17e8caf9e957d
bxtx999 <me@hoooo.org>
2021-05-05 21:22:14 +0800

    update examples

---
+++ example.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "Arachne/Arachne.h"
+
+void numberPrinter(int n) {
+    printf("NumberPrinter says %d\n", n);
+}
+
+// This is where user code should start running.
+void AppMain(int argc, const char** argv) {
+    printf("Arachne says hello world and creates a thread.\n");
+    auto tid = Arachne::createThread(numberPrinter, 5);
+    Arachne::join(tid);
+}
+
+// The following bootstrapping code should be copied verbatim into most Arachne
+// applications.
+void AppMainWrapper(int argc, const char** argv) {
+    AppMain(argc, argv);
+    Arachne::shutDown();
+}
+
+int main(int argc, const char** argv){
+    Arachne::init(&argc, argv);
+    Arachne::createThread(&AppMainWrapper, argc, argv);
+    Arachne::waitForTermination();
+    return 0;
+}
